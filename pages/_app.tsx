7d069018d94f27e507ebc91489424a30b84733c7
Jupalaja <jupalaja@gmail.com>
2024-03-18 20:59:43 -0500

    Added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { QuestionsProvider } from "@/contexts";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
     <QuestionsProvider>
-      <Component {...pageProps} />
+    <Component {...pageProps} />
     </QuestionsProvider>
+    <Analytics />
+    </>
   );
 }
